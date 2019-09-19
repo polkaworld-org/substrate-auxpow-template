bd6bad698f66f585a5ca11026e3b0e66e3fdb077
Andrew yz <deedecho@gmail.com>
2019-09-19 16:39:28 +0800

    Implement the parity codec for AuxPow

--- auxpow/src/btc_tx.rs
+++ auxpow/src/btc_tx.rs
@@ -1,25 +1,8 @@
 use primitives::H256;
-use codec::{Decode, Encode, IoReader};
-use rustc_hex::{FromHex, FromHexError};
+use codec::{Decode, Encode, Input, Output, IoReader, Error};
+use rustc_hex::{FromHex};
 use std::{ str };
 use crate::btc_hash::dhash256;
-
-#[derive(Debug, Encode, Decode)]
-pub struct Bytes(Vec<u8>);
-
-impl From<&'static str> for Bytes {
-	fn from(s: &'static str) -> Self {
-		s.parse().unwrap()
-	}
-}
-
-impl str::FromStr for Bytes {
-	type Err = FromHexError;
-
-	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		s.from_hex().map(Bytes)
-	}
-}
 
 #[derive(Debug, Encode, Decode)]
 pub struct OutPoint {
@@ -27,26 +10,120 @@ pub struct OutPoint {
 	pub index: u32,
 }
 
-#[derive(Debug, Encode, Decode)]
+#[derive(Debug)]
 pub struct TransactionInput {
 	pub previous_output: OutPoint,
-	pub script_sig: Bytes,
+	pub script_sig: Vec<u8>,
 	pub sequence: u32,
-	pub script_witness: Vec<Bytes>,
 }
 
-#[derive(Debug, Encode, Decode)]
+impl Encode for TransactionInput {
+	fn encode_to<T: Output>(&self, dest: &mut T) {
+		self.previous_output.encode_to(dest);
+		(self.script_sig.len() as u8).encode_to(dest);
+		for i in 0..self.script_sig.len() {
+			self.script_sig[i].encode_to(dest);
+		}
+
+		self.sequence.encode_to(dest);
+	}
+}
+
+impl Decode for TransactionInput {
+	fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
+		let previous_output: OutPoint = Decode::decode(input)?;
+		let len: u8 = Decode::decode(input)?;
+		let mut script_sig: Vec<u8> = Vec::with_capacity(len as usize);
+		for _ in 0..len {
+			script_sig.push(Decode::decode(input)?);
+		}
+		let sequence: u32 = Decode::decode(input)?;
+
+		Ok(TransactionInput {
+			previous_output,
+			script_sig,
+			sequence,
+		})
+	}
+}
+
+#[derive(Debug)]
 pub struct TransactionOutput {
 	pub value: u64,
-	pub script_pubkey: Bytes,
+	pub script_pubkey: Vec<u8>,
 }
 
-#[derive(Debug, Encode, Decode)]
+impl Encode for TransactionOutput {
+	fn encode_to<T: Output>(&self, dest: &mut T) {
+		self.value.encode_to(dest);
+		(self.script_pubkey.len() as u8).encode_to(dest);
+		for i in 0..self.script_pubkey.len() {
+			self.script_pubkey[i].encode_to(dest);
+		}
+	}
+}
+
+impl Decode for TransactionOutput {
+	fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
+		let value: u64 = Decode::decode(input)?;
+		let len: u8 = Decode::decode(input)?;
+		let mut script_pubkey: Vec<u8> = Vec::with_capacity(len as usize);
+		for _ in 0..len {
+			script_pubkey.push(Decode::decode(input)?);
+		}
+
+		Ok(TransactionOutput {
+			value,
+			script_pubkey,
+		})
+	}
+}
+
+#[derive(Debug)]
 pub struct BtcTx {
 	pub version: i32,
 	pub inputs: Vec<TransactionInput>,
 	pub outputs: Vec<TransactionOutput>,
 	pub lock_time: u32,
+}
+
+impl Encode for BtcTx {
+	fn encode_to<T: Output>(&self, dest: &mut T) {
+		self.version.encode_to(dest);
+		(self.inputs.len() as u8).encode_to(dest);
+		for i in 0..self.inputs.len() {
+			self.inputs[i].encode_to(dest);
+		}
+		(self.outputs.len() as u8).encode_to(dest);
+		for i in 0..self.outputs.len() {
+			self.outputs[i].encode_to(dest);
+		}
+		self.lock_time.encode_to(dest);
+	}
+}
+
+impl Decode for BtcTx {
+	fn decode<I: Input>(src: &mut I) -> Result<Self, Error> {
+		let version: i32 = Decode::decode(src)?;
+		let input_len: u8 = Decode::decode(src)?;
+		let mut inputs: Vec<TransactionInput> = Vec::with_capacity(input_len as usize);
+		for _ in 0..input_len {
+			inputs.push(Decode::decode(src)?);
+		}
+		let output_len: u8 = Decode::decode(src)?;
+		let mut outputs: Vec<TransactionOutput> = Vec::with_capacity(output_len as usize);
+		for _ in 0..output_len {
+			outputs.push(Decode::decode(src)?);
+		}
+		let lock_time: u32 = Decode::decode(src)?;
+
+		Ok(BtcTx {
+			version,
+			inputs,
+			outputs,
+			lock_time,
+		})
+	}
 }
 
 impl BtcTx {
@@ -57,18 +134,15 @@ impl BtcTx {
 
 impl From<&'static str> for BtcTx {
 	fn from(s: &'static str) -> Self {
-		println!("1");
 		let hex_data = &s.from_hex::<Vec<u8>>().unwrap();
-		println!("2");
         let mut io_reader = IoReader(std::io::Cursor::new(hex_data as &[u8]));
-		println!("3");
         Decode::decode(&mut io_reader).unwrap()
 	}
 }
 
 #[cfg(test)]
 mod tests {
-	use super::{BtcTx, TransactionInput, OutPoint, TransactionOutput};
+	use super::{BtcTx};
 	use primitives::H256;
     use std::str::FromStr;
 
@@ -78,63 +152,22 @@ mod tests {
 	#[test]
 	fn test_transaction_reader() {
 		let t: BtcTx = "0100000001a6b97044d03da79c005b20ea9c0e1a6d9dc12d9f7b91a5911c9030a439eed8f5000000004948304502206e21798a42fae0e854281abd38bacd1aeed3ee3738d9e1446618c4571d1090db022100e2ac980643b0b82c0e88ffdfec6b64e3e6ba35e7ba5fdd7d5d6cc8d25c6b241501ffffffff0100f2052a010000001976a914404371705fa9bd789a2fcd52d2c580b65d35549d88ac00000000".into();
+		assert_eq!(t.version, 1);
+		assert_eq!(t.lock_time, 0);
+		assert_eq!(t.inputs.len(), 1);
+		assert_eq!(t.outputs.len(), 1);
+		let tx_input = &t.inputs[0];
+		assert_eq!(tx_input.sequence, 4294967295);
+		let tx_output = &t.outputs[0];
+		assert_eq!(tx_output.value, 5000000000);
 
-	 	println!("{:#?}", t);
-	
-		// assert_eq!(t.version, 1);
-		// assert_eq!(t.lock_time, 0);
-		// assert_eq!(t.inputs.len(), 1);
-		// assert_eq!(t.outputs.len(), 1);
-		// let tx_input = &t.inputs[0];
-		// assert_eq!(tx_input.sequence, 4294967295);
-		// assert_eq!(tx_input.script_sig, "48304502206e21798a42fae0e854281abd38bacd1aeed3ee3738d9e1446618c4571d1090db022100e2ac980643b0b82c0e88ffdfec6b64e3e6ba35e7ba5fdd7d5d6cc8d25c6b241501".into());
-		// let tx_output = &t.outputs[0];
-		// assert_eq!(tx_output.value, 5000000000);
-		// assert_eq!(tx_output.script_pubkey, "76a914404371705fa9bd789a2fcd52d2c580b65d35549d88ac".into());
+	 	// println!("{:?}", t);
 	}
 
-	// #[test]
-	// fn test_transaction_hash() {
-	// 	let t: BtcTx = "0100000001a6b97044d03da79c005b20ea9c0e1a6d9dc12d9f7b91a5911c9030a439eed8f5000000004948304502206e21798a42fae0e854281abd38bacd1aeed3ee3738d9e1446618c4571d1090db022100e2ac980643b0b82c0e88ffdfec6b64e3e6ba35e7ba5fdd7d5d6cc8d25c6b241501ffffffff0100f2052a010000001976a914404371705fa9bd789a2fcd52d2c580b65d35549d88ac00000000".into();
-	// 	let hash = H256::from_reversed_str("5a4ebf66822b0b2d56bd9dc64ece0bc38ee7844a23ff1d7320a88c5fdb2ad3e2");
-	// 	assert_eq!(t.hash(), hash);
-	// }
-
-	// #[test]
-	// fn test_transaction_reader_with_witness() {
-	// 	// test case from https://github.com/bitcoin/bips/blob/master/bip-0143.mediawiki
-	// 	let actual: BtcTx = "01000000000102fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f00000000494830450221008b9d1dc26ba6a9cb62127b02742fa9d754cd3bebf337f7a55d114c8e5cdd30be022040529b194ba3f9281a99f2b1c0a19c0489bc22ede944ccf4ecbab4cc618ef3ed01eeffffffef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a0100000000ffffffff02202cb206000000001976a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac9093510d000000001976a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac000247304402203609e17b84f6a7d30c80bfa610b5b4542f32a8a0d5447a12fb1366d7f01cc44a0220573a954c4518331561406f90300e8f3358f51928d43c212a8caed02de67eebee0121025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee635711000000".into();
-	// 	let expected = BtcTx {
-	// 		version: 1,
-	// 		inputs: vec![TransactionInput {
-	// 			previous_output: OutPoint {
-	// 				hash: "fff7f7881a8099afa6940d42d1e7f6362bec38171ea3edf433541db4e4ad969f".into(),
-	// 				index: 0,
-	// 			},
-	// 			script_sig: "4830450221008b9d1dc26ba6a9cb62127b02742fa9d754cd3bebf337f7a55d114c8e5cdd30be022040529b194ba3f9281a99f2b1c0a19c0489bc22ede944ccf4ecbab4cc618ef3ed01".into(),
-	// 			sequence: 0xffffffee,
-	// 			script_witness: vec![],
-	// 		}, TransactionInput {
-	// 			previous_output: OutPoint {
-	// 				hash: "ef51e1b804cc89d182d279655c3aa89e815b1b309fe287d9b2b55d57b90ec68a".into(),
-	// 				index: 1,
-	// 			},
-	// 			script_sig: "".into(),
-	// 			sequence: 0xffffffff,
-	// 			script_witness: vec![
-	// 				"304402203609e17b84f6a7d30c80bfa610b5b4542f32a8a0d5447a12fb1366d7f01cc44a0220573a954c4518331561406f90300e8f3358f51928d43c212a8caed02de67eebee01".into(),
-	// 				"025476c2e83188368da1ff3e292e7acafcdb3566bb0ad253f62fc70f07aeee6357".into(),
-	// 			],
-	// 		}],
-	// 		outputs: vec![TransactionOutput {
-	// 			value: 0x0000000006b22c20,
-	// 			script_pubkey: "76a9148280b37df378db99f66f85c95a783a76ac7a6d5988ac".into(),
-	// 		}, TransactionOutput {
-	// 			value: 0x000000000d519390,
-	// 			script_pubkey: "76a9143bde42dbee7e4dbe6a21b2d50ce2f0167faa815988ac".into(),
-	// 		}],
-	// 		lock_time: 0x00000011,
-	// 	};
-	// 	assert_eq!(actual, expected);
-	// }
+	#[test]
+	fn test_transaction_hash() {
+		let t: BtcTx = "0100000001a6b97044d03da79c005b20ea9c0e1a6d9dc12d9f7b91a5911c9030a439eed8f5000000004948304502206e21798a42fae0e854281abd38bacd1aeed3ee3738d9e1446618c4571d1090db022100e2ac980643b0b82c0e88ffdfec6b64e3e6ba35e7ba5fdd7d5d6cc8d25c6b241501ffffffff0100f2052a010000001976a914404371705fa9bd789a2fcd52d2c580b65d35549d88ac00000000".into();
+		let hash = H256::from_str("e2d32adb5f8ca820731dff234a84e78ec30bce4ec69dbd562d0b2b8266bf4e5a").unwrap();
+		assert_eq!(t.hash(), hash);
+	}
 }
--- auxpow/src/lib.rs
+++ auxpow/src/lib.rs
@@ -2,21 +2,19 @@ pub mod btc_header;
 pub mod btc_tx;
 pub mod btc_hash;
 
-
 use primitives::H256;
-// use codec::{Decode, Encode};
+use codec::{Decode, Encode, Input, Output, IoReader, Error};
 use crate::btc_header::BtcHeader;
 use crate::btc_tx::BtcTx;
+use rustc_hex::FromHex;
 
-// #[derive(Encode, Decode)]
+#[derive(Debug)]
 pub struct AuxPow{
-    /// Hash of the parent_header.
-    pub parent_hash: H256,
-    /// Parent block header.
-    pub parent_header: BtcHeader,
     /// Coinbase transaction that is in the parent block, linking the AuxPOW 
     /// block to its parent block.
     pub coinbase_txn: BtcTx,
+    /// Hash of the parent_header.
+    pub parent_hash: H256,
     /// The merkle branch linking the coinbase_txn to the parent block's 
     /// merkle_root.
     pub coinbase_branch: Vec<H256>,
@@ -30,6 +28,65 @@ pub struct AuxPow{
     pub blockchain_branch: Vec<H256>,
     /// Bitmask for blockchain_branch.
     pub blockchain_index: u32,
+    /// Parent block header.
+    pub parent_header: BtcHeader,
+}
+
+impl Encode for AuxPow {
+	fn encode_to<T: Output>(&self, dest: &mut T) {
+		self.coinbase_txn.encode_to(dest);
+		self.parent_hash.encode_to(dest);
+		(self.coinbase_branch.len() as u8).encode_to(dest);
+		for i in 0..self.coinbase_branch.len() {
+			self.coinbase_branch[i].encode_to(dest);
+		}
+        self.coinbase_index.encode_to(dest);
+        (self.blockchain_branch.len() as u8).encode_to(dest);
+		for i in 0..self.blockchain_branch.len() {
+			self.blockchain_branch[i].encode_to(dest);
+		}
+        self.blockchain_index.encode_to(dest);
+        self.parent_header.encode_to(dest);
+	}
+}
+
+impl Decode for AuxPow {
+	fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
+		let coinbase_txn: BtcTx = Decode::decode(input)?;
+		let parent_hash: H256 = Decode::decode(input)?;
+		let len: u8 = Decode::decode(input)?;
+		let mut coinbase_branch: Vec<H256> = Vec::with_capacity(len as usize);
+		for _ in 0..len {
+			coinbase_branch.push(Decode::decode(input)?);
+		}
+        let coinbase_index: u32 = Decode::decode(input)?;
+
+        let len: u8 = Decode::decode(input)?;
+		let mut blockchain_branch: Vec<H256> = Vec::with_capacity(len as usize);
+		for _ in 0..len {
+			blockchain_branch.push(Decode::decode(input)?);
+		}
+        let blockchain_index: u32 = Decode::decode(input)?;
+        let parent_header: BtcHeader = Decode::decode(input)?;
+
+		Ok(AuxPow {
+			coinbase_txn,
+            parent_hash,
+            coinbase_branch,
+            coinbase_index,
+            blockchain_branch,
+            blockchain_index,
+            parent_header,
+		})
+	}
+}
+
+impl From<&'static str> for AuxPow {
+	fn from(s: &'static str) -> Self {
+        let hex_data = &s.from_hex::<Vec<u8>>().unwrap();
+        let mut io_reader = IoReader(std::io::Cursor::new(hex_data as &[u8]));
+        Decode::decode(&mut io_reader).unwrap()
+	}
 }
 
 #[cfg(test)]
@@ -38,6 +95,8 @@ mod tests {
 
     #[test]
     fn auxpow_decode() {
-        
+        let auxpow: AuxPow = "02000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4b039aff0904db044a5b742f4254432e434f4d2ffabe6d6d35ecfc5f5ca2971449ee78b7d810f280de7e3e7c407e3c0162ef8692df350ef8020000004204cb9a011fde202e00000000000000ffffffff0200000000000000001976a914c0174e89bd93eacd1d5a1af4ba1802d412afc08688ac0000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf9000000001d1879510258c5186e39cfcde4539c88686854b1ca640681dd38ed9527e635600000000000015f2f03802d61504f12e25d4b679b881ddb374cc04f240b6eb765d887679fb6360000000000000020a9f32bdb09d7777f3fa308fcd221e531393441f50e7f8b2d4ef63b2c3440940ec866338e7674b07d6a92269317f09f6c0fdb60ce7052e0211133e0015727ebb2db044a5bffff7f20db044a5b".into();
+        // println!("{:#?}", auxpow);
     }
 }
+ 
